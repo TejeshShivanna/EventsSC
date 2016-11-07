b5253caedba6f2ec3e1e67fad5be3f09dbd1577c
Ishaan Berry <berry.ishaan@gmail.com>
2016-11-07 00:15:51 -0800

    adding config

---
+++ BackEnd/Service/src/main/java/eventssc/config/BeanConfig.java
@@ -0,0 +1,19 @@
+package eventssc.config;
+
+import eventssc.database.AmazonRDS;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class BeanConfig {
+
+    @org.springframework.context.annotation.Bean
+    public AmazonRDS amazonRDS(@Value("${aws.host}") String awshost,
+                               @Value("${aws.port}") int awsport,
+                               @Value("${aws.database}") String awsdatabase,
+                               @Value("${aws.username}") String awsusername,
+                               @Value("${aws.password}") String awspassword){
+        return new AmazonRDS(awshost, awsdatabase, awsusername, awspassword, awsport);
+    }
+}
